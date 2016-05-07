f3642bbe74ccfa52b0af93eaa8e66f9c3bbfab74
Hiroaki Yutani <yutani.ini@gmail.com>
2016-05-06 22:17:48 -0400

    improve script

--- library/cran.py
+++ library/cran.py
@@ -20,18 +20,18 @@ options:
     description:
       - The repository
     required: false
-    default: "http://cran.rstudio.com/"
+    default: "https://cran.rstudio.com/"
 '''
 
 RSCRIPT = '/usr/bin/Rscript'
 
 def get_installed_version(module):
 	cmd = [RSCRIPT, '--slave', '--no-save', '--no-restore-history',
-		'-e', 'cat(packageVersion("{name:}"))'.format(
+		'-e', 'p <- installed.packages(); cat(p[p[,1] == "{name:}",3])'.format(
 			name = module.params['name']
 		)]
-	(rc, out, err) = module.run_command(cmd, check_rc=True)
-	return out[1].strip() if not err else None
+	(rc, stdout, stderr) = module.run_command(cmd, check_rc=False)
+	return stdout.strip() if rc == 0 else None
 
 
 def install(module):
@@ -40,7 +40,8 @@ def install(module):
 			name = module.params['name'],
 			repos = module.params['repo']
 		)]
-	module.run_command(cmd, check_rc=True)
+	(rc, stdout, stderr) = module.run_command(cmd, check_rc=True)
+	return stderr
 
 
 def uninstall(module):
@@ -48,7 +49,8 @@ def uninstall(module):
 		'-e', 'remove.packages(pkgs="{name:}")'.format(
 			name = module.params['name']
 		)]
-	module.run_command(cmd, check_rc=True)
+	(rc, stdout, stderr) = module.run_command(cmd, check_rc=True)
+	return stderr
 
 
 def main():
@@ -56,17 +58,30 @@ def main():
 	    argument_spec = dict(
 		state     = dict(default='present', choices=['present', 'absent']),
 		name      = dict(required=True),
-		repo      = dict(default='http://cran.rstudio.com/')
+		repo      = dict(default='https://cran.rstudio.com/')
 	    )
 	)
 	state   = module.params['state']
+	name    = module.params['name']
 	changed = False
+	version = get_installed_version(module)
 
-	if state == 'present':
-		install(module)
+	if state == 'present' and not version:
+		stderr = install(module)
+		version = get_installed_version(module)
+		if not version:
+			module.fail_json(msg='Failed to install {name:}: {err:}'.format(name = name, err  = stderr, version = version))
 		changed = True
 
-	module.exit_json(changed=changed, name=module.params['name'])
+	elif state == 'absent' and version:
+		stderr = uninstall(module)
+		version = get_installed_version(module)
+		if version:
+			module.fail_json(msg='Failed to install {name:}: {err:}'.format(name = name, err  = stderr))
+		changed = True
+
+	module.exit_json(changed=changed, name=name, version=version)
+
 
 from ansible.module_utils.basic import *
 if __name__ == '__main__':
